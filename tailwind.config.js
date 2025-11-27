b799239ab5a5238c40be1141e217c6cf69687a76
nitnav01 <66711203+nitnav01@users.noreply.github.com>
2025-11-28 01:13:39 +0530

    Initial Setup

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        serif: ['"Playfair Display"', 'serif'],
+        sans: ['"Inter"', 'sans-serif'],
+      },
+      colors: {
+        cream: '#FDFCF8',
+        forest: '#0A332B',
+        gold: '#D4AF37',
+      }
+    },
+  },
+  plugins: [],
+}
